bd9caef0b566c1c7bc8b95b45eb8cbd20d272267
Basia <piechotaba@gmail.com>
2023-10-18 07:36:40 +0200

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+        ChromeDriver driver = new ChromeDriver();
+
+    }
+}
