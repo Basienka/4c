ee43df24e6cc53c07af38e10a6f7d1478c478a1a
Basia <piechotaba@gmail.com>
2023-10-18 08:55:06 +0200

    init

---
+++ src/main/java/zoo/Animal.java
@@ -0,0 +1,17 @@
+package zoo;
+
+public class Animal {
+    String specie;
+    int age;
+    double height; //liczba zmiennoprzecinkowa
+    long hairCount; //liczba calkowita
+    float weight; //liczba zmiennoprzecinkowa
+    boolean isAlive; //zmienna logiczna true/false
+
+    public void speak() {
+        //sout
+        //soutv
+        System.out.println("");
+        System.out.println("hello" + specie);
+    }
+}
---
+++ src/main/java/zoo/Owner.java
@@ -0,0 +1,10 @@
+package zoo;
+
+public class Owner {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.specie = "Chicken";
+        animal.speak();
+    }
+
+    }
